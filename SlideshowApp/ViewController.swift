f9f2415b6314a5a7cfd3f18ab0012d139015da2e
高野翔 <ShoTakano@43x232x203x168.ap43.ftth.ucom.ne.jp>
2018-08-18 21:49:25 +0900

    途中経過

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,14 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImage: UIImageView!
+    var photo1: UIImage!
+    var photo2: UIImage!
+    var photo3: UIImage!
+    var photo4: UIImage!
+    var photo5: UIImage!
+    var photo6: UIImage!
+    var count: Int = 1
+    
+    @IBAction func tapAction(_ sender: Any) {
+        performSegue(withIdentifier: "GoToBigPhoto", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        photo1 = UIImage(named:"photo1")
+        photo2 = UIImage(named:"photo2")
+        photo3 = UIImage(named:"photo3")
+        photo4 = UIImage(named:"photo4")
+        photo5 = UIImage(named:"photo5")
+        photo6 = UIImage(named:"photo6")
+        photoImage.image = UIImage(named:"photo\(count)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func toNext(_ sender: Any) {
+        self.count += 1
+        self.photoImage.image = UIImage(named:"photo\(count)")
+        if count > 6 {
+            self.count = 1
+            self.photoImage.image = UIImage(named:"photo\(count)")
+        }
+    }
+    
+    @IBAction func toBack(_ sender: Any) {
+        self.count -= 1
+        self.photoImage.image = UIImage(named:"photo\(count)")
+        if count < 1 {
+            self.count = 6
+            self.photoImage.image = UIImage(named:"photo\(count)")
+        }
+    }
+    
+    @IBAction func StartAndStopShow(_ sender: Any) {
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let viewControllerBigPhoto : ViewControllerBigPhoto = segue.destination as! ViewControllerBigPhoto
+        viewControllerBigPhoto.countBig = count
     }
 
 
---
+++ SlideshowApp/ViewControllerBigPhoto.swift
@@ -0,0 +1,43 @@
+//
+//  ViewControllerBigPhoto.swift
+//  SlideshowApp
+//
+//  Created by 高野翔 on 2018/08/18.
+//  Copyright © 2018年 高野翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerBigPhoto: UIViewController {
+
+    @IBOutlet weak var buttonToBack: UIButton!
+    @IBOutlet weak var bigPhotoImage: UIImageView!
+    var countBig: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buttonToBack.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        buttonToBack.layer.borderColor = UIColor.blue.cgColor
+        buttonToBack.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        buttonToBack.layer.borderWidth = 2.0
+        buttonToBack.layer.cornerRadius = 10.0 //丸みを数値で変更できる
+        bigPhotoImage.image = UIImage(named:"photo\(countBig)")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
